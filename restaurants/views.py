9e43f67d3437a15d3f85c7d6ce6ca87b2d97506c
Danny_from_nb <cooldannyboy@gmail.com>
2015-11-19 17:53:51 +0800

    error checking

--- restaurants/views.py
+++ restaurants/views.py
@@ -52,11 +52,16 @@ def comment(request, id):
         content = request.POST['content']
         email = request.POST['email']
         date_time = timezone.localtime(timezone.now())
-        Comment.objects.create(
-            visitor=visitor,
-            email=email,
-            content=content,
-            date_time=date_time,
-            restaurant=r
-        )
+
+        error = any(not request.POST[k] for k in request.POST)
+
+        if not error:
+            Comment.objects.create(
+                visitor=visitor,
+                email=email,
+                content=content,
+                date_time=date_time,
+                restaurant=r
+            )
+
     return render_to_response('comments.html', RequestContext(request, locals()))
